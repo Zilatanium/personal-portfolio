a169b2ae70f3ebf693cf12dae31d3220050dabde
Zilatanium <josephdelgado1051@gmail.com>
2022-08-20 18:41:43 -0400

    updated banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,58 +1,69 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg"
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 import { useState, useEffect } from "react";
 
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
     const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300 - Math.random()*100);
-    const [index, setIndex] = useState(1);
+    const [delta, setDelta] = useState(250 - Math.random() * 100);
     const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
     const period = 2000;
 
-    useEffect (() => {
+    useEffect(() => {
         let ticker = setInterval(() => {
-            ticker();
+            tick();
         }, delta)
-        
-        return () => { clearInterval(ticker)};
+
+        return () => { clearInterval(ticker) };
     }, [text])
 
-    const tick =() => {
+    const tick = () => {
         let i = loopNum % toRotate.length;
         let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1 );
-        
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
         setText(updatedText);
 
         if (isDeleting) {
-            setDelta(prevDelta => prevDelta/2);
+            setDelta(prevDelta => prevDelta / 1000);
         }
 
         if (!isDeleting && updatedText === fullText) {
             setIsDeleting(true);
+
             setDelta(period);
-        } else if(isDeleting && updatedText === '') {
+        } else if (isDeleting && updatedText === '') {
             setIsDeleting(false);
             setLoopNum(loopNum + 1);
-            setDelta(500);
-        }
+            setDelta(100);
+        } 
     }
     return (
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                        <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm Joseph!`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, eos perferendis quos consequatur exercitationem itaque!</p>
-                        <button onClick={()=> console.log('connect')}>Let's connect!<ArrowRightCircle size={25} /></button>
+                        <TrackVisibility>
+                            {({ isVisible }) =>
+                                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                                    <span className="tagline">Welcome to my Portfolio</span>
+                                    <h1>{`Hi I'm Joseph! `}</h1>
+                                    <h1>{`< `}<span className="wrap">{text} {` >`}</span></h1>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, eos perferendis quos consequatur exercitationem itaque!</p>
+                                    <button onClick={() => console.log('connect')}>Let's connect!<ArrowRightCircle size={25} /></button>
+                                </div>}
+                        </TrackVisibility>
                     </Col>
-                    <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt="Header Img" />
-                    </Col>
+                    <TrackVisibility>
+                        {({ isVisible }) =>
+                            <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                                <img src={headerImg} alt="Header Img" />
+                            </div>}
+                    </TrackVisibility>
                 </Row>
             </Container>
         </section>
