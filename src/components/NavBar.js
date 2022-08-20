bcad9e5e6d077ed0ea99366fe3de0c5f9242f6f9
Zilatanium <josephdelgado1051@gmail.com>
2022-08-20 15:26:30 -0400

    Add all files

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import { NavBar, Container } from "react-bootstrap";
+export const NavBar = () => {
+    return (
+        <Navbar expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src={''} alt="Logo"/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <span className="navbar-toggler-icon"></span>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#skill">Skills</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                    </Nav>
+                    <span className="navbar-text">
+                        <div className="social-icon">
+                            <a href="#"><img src={''} alt=""/></a>
+                            <a href="#"><img src={''} alt=""/></a>
+                            <a href="#"><img src={''} alt=""/></a>
+                        </div>
+                        <button className="vvd" onClick={()=> console.log('connect')}><span>Let's Connect</span></button>
+                    </span>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
