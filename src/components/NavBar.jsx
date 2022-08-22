7984a2e64a13efc570f6434c0fb531718aa81e41
Joseph <josephdelgado1051@gmail.com>
2022-08-22 09:26:19 -0400

    icon update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from '../assets/img/logo.png';
-import { HashLink } from 'react-router-hash-link';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
@@ -48,7 +47,7 @@ export const NavBar = () => {
                     <span className="navbar-text">
                         <div className="social-icon">
                             <a href="https://www.linkedin.com/in/joseph-delgado-0798a71b5/"><img src={navIcon1} alt="" /></a>
-                            <a href="https://www.w3schools.com/"><img src={navIcon2} alt="" /></a>
+                            <a href="https://github.com/Zilatanium/"><img src={navIcon2} alt="" /></a>
                             <a href="https://www.w3schools.com/"><img src={navIcon3} alt="" /></a>
                         </div>
                             <a href="#connect"><button className="vvd"><span>Let’s Connect</span></button></a>
