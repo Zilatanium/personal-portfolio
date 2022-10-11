0cfe9af4f0a71cbb132ae62b67ab4fcaf88aa354
Joseph Delgado <94134670+Zilatanium@users.noreply.github.com>
2022-10-11 12:47:24 -0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -48,7 +48,7 @@ export const NavBar = () => {
                         <div className="social-icon">
                             <a href="https://www.linkedin.com/in/joseph-delgado-0798a71b5/"><img src={navIcon1} alt="linkedin" /></a>
                             <a href="https://github.com/Zilatanium/"><img src={navIcon2} alt="github" /></a>
-                            <a href="https://drive.google.com/file/d/1pxVIYxLmrQ1TwyfxZTzRBBBCoKKT_-qQ/view?usp=sharing"><img src={navIcon3} alt="resume" /></a>
+                            <a href="https://drive.google.com/file/d/1ITnR4JpvixpI2A5mga0Yc31k7XV2Oa7p/view?usp=sharing"><img src={navIcon3} alt="resume" /></a>
                         </div>
                             <a href="#connect"><button className="vvd"><span>Let’s Connect</span></button></a>
                     </span>
